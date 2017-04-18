a1a7926a207f08ef84be4e1d3aeb744b1aed5ddd
Christos Koutsiaris <koutsiaris@gmail.com>
2017-04-18 22:09:38 +0100

    add router

---
+++ src/Error404.js
@@ -0,0 +1,14 @@
+/*import React from 'react';
+export const Error404 = () => (
+    <div>
+        <h4>Sorry, this page doesn't exist</h4>
+    </div>
+)*/
+
+import React from 'react';
+
+export const Error404 = () => (
+<div>
+        <h4>Sorry, this page doesn't exist</h4>
+    </div>
+);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './App';
+import {Error404} from './Error404'
 import './index.css';
 import 'bulma/css/bulma.css';
+import {Router, Route, hashHistory} from 'react-router'
 
 ReactDOM.render(
     <div className="container">
-    <App/>
-    </div>, document.getElementById('root'));
+    <Router history={hashHistory}>
+        <Route path="/" component={App}/>
+        <Route path="*" component={Error404}/>
+    </Router>
+</div>, document.getElementById('root'));
